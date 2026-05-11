45ddf12ef028cc573c3e501d7fcbad5706c43d04
Pranav-Dhadwal <dhadwal.pranav01@gmail.com>
2026-05-12 02:19:50 +0530

    finalized data and data ingestion with test

---
+++ tests/test_ingestion.py
@@ -0,0 +1,9 @@
+from customer_churn.components.data_ingestion import DataIngestion
+
+
+if __name__ == "__main__":
+    obj = DataIngestion()
+
+    train_path, test_path = obj.initiate_data_ingestion()
+
+    print(train_path, test_path)
